e82a2c372146826cede2e6fecb18897d086d9ba0
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-07-30 10:46:41 +0545

    Refactor history.js and add store.js***

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,15 +1,21 @@
-import store from "./store.js";
+import store from "./store/store.js";
 import render from "./renderer.js";
 
 export function navigateTo(path, state) {
-  path = path.includes(store.baseURL) ? path : store.baseURL + path;
-  window.history.pushState(state || {}, "", path);
+  const newPath = path.includes(store.baseURL) ? path : store.baseURL + path;
+  window.history.pushState(state || {}, "", newPath);
+  render();
+}
+
+export function navigateByReplace(path, state) {
+  const newPath = path.includes(store.baseURL) ? path : store.baseURL + path;
+  window.history.replaceState(state || {}, "", newPath);
   render();
 }
 
 export function replaceState(path, state) {
-  path = path.includes(store.baseURL) ? path : store.baseURL + path;
-  window.history.replaceState(state || {}, "", path);
+  const newPath = path.includes(store.baseURL) ? path : store.baseURL + path;
+  window.history.replaceState(state || {}, "", newPath);
 }
 
 export function handleHistoryPopState() {
--- assets/js/store/store.js
+++ assets/js/store/store.js
@@ -1,4 +1,4 @@
-import data from "../../data.json" assert { type: "json" };
+import data from "../../../data.json" assert { type: "json" };
 
 class Store {
   constructor() {
