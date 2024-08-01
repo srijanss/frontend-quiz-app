ea42da25db664e0c1a88c3dad3dbf1239a3d23c4
Srijan Manandhar <srijan.manandhar@gmail.com>
2024-08-01 12:30:03 +0545

    Remove console.log statement in renderer.js

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -2,7 +2,6 @@ import store from "./store/store.js";
 import { matchRoute } from "./router.js";
 
 export default function render() {
-  console.log(window.history);
   const path = window.location.pathname;
   const pathWithoutBase = path.replace(store.baseURL, "/");
   const match = matchRoute(pathWithoutBase);
